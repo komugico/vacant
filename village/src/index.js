82df71cea8dbe915c06159e92a1a5fa6a9cbe304
komugico <shu.isaka.pc+develop@gmail.com>
2021-05-22 22:27:20 +0900

    不要なimportを消去

--- village/src/index.js
+++ village/src/index.js
@@ -3,8 +3,7 @@
 /* ========================================================================== */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from "react-router-dom";
 
 /* ========================================================================== */
 /* Service Worker                                                             */
